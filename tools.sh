f1392752c3eea602065031353aee319b4a54405d
jakubmajer@live.com <jakubmajer@live.com>
2024-04-28 15:26:04 +0200

    .

--- tools.sh
+++ tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.28.0/crictl-v1.28.0-linux-amd64.tar.gz 
 tar xzfv crictl-v1.28.0-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-v1.28.0-linux-amd64.tar.gz
